7119e3ad802aa57a563c7529ada492fc13d9721a
Serrantes <aserrantes@nps.gov>
2026-07-31 17:25:06 -0700

    Update tests for loadAndWrangleMOJNAspen

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,25 +1,20 @@
 
 
-# Things to test in fetchAndWrangleAspen - only function in utils rn
-  # check that $data and $meta data exists
-  # check that there are the correct number of tables in data and metadata
-  # check that each data table has the correct number of columns and that the columns have the right names
-  # check that the return type from function is correct
 
 data <- loadAndWrangleMOJNAspen()
 
 test_that("Test that loadAndWrangleMOJNAspen() works", {
-  # Compare expected and actual names in return object
-  actual_names <- names(data)
-  expected_names <- c("data", "metadata")
-  expect_equal(actual_names, expected_names)
+  # Compare expected and actual lists in return object
+  actual_list <- names(data)
+  expected_list <- "data"
+  expect_equal(actual_list, expected_list)
 
   # Compare expected and actual names of the aspen data frames
-  actual_cols <- names(data$data)
-  expected_cols <- c("AllSites", "SiteVisit", "Disturbances", "Observations", "Pests")
-  expect_equal(actual_cols, expected_cols)
+  actual_tbls <- names(data$data)
+  expected_tbls <- c("SiteVisit", "Disturbances", "Observations", "Pests")
+  expect_equal(actual_tbls, expected_tbls)
 
-  # Check that the first object in the data list is a database
+  # Check that the first object in the data list is a dataframe
   returnType <- data$data[[1]]
   expect_s3_class(returnType, "data.frame")
 })
