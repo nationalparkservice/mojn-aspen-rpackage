e576209e634ceb02ffd0ec64db064c55285a8dbf
Serrantes <aserrantes@nps.gov>
2026-08-13 15:56:49 -0700

    Update testthat expect fx used

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,5 +1,5 @@
 
-# MOJN Wrangle function tests ----
+# MOJN Wrangle and Package function tests ----
 # Read in saved MOJN data for testing - object has both db and sites tbls and went through fetchagol::cleanData()
 mojn_wrangle_test_data <- test_path("mojn_wrangle_test_data.rds")
 
@@ -16,23 +16,22 @@ test_that("wrangleSites works", {
   expect_false(any(stringr::str_detect(names(mojn_wrangled_sites), "_$")))
 
   # Test that all shift values are expanded
-  expect_true(all(unique(mojn_wrangled_sites$data$AllSites$shift) %in% c("Not Shifted", "North", "South", "East", "West", NA_character_)))
+  expect_all_true(unique(mojn_wrangled_sites$data$AllSites$shift) %in% c("Not Shifted", "North", "South", "East", "West", NA_character_))
 
   # Test new geographic columns
-  expect_true(all(unique(mojn_wrangled_sites$data$AllSites$verbatimCoordinateSystem) %in% c("UTM", NA_character_)))
-  expect_true(all(unique(mojn_wrangled_sites$data$AllSites$verbatimSRS) %in% c("EPSG:4269", NA_character_)))
-  expect_true(all(unique(mojn_wrangled_sites$data$AllSites$geodeticDatum) %in% c("EPSG:4326", NA_character_)))
+  expect_all_true(unique(mojn_wrangled_sites$data$AllSites$verbatimCoordinateSystem) %in% c("UTM", NA_character_))
+  expect_all_true(unique(mojn_wrangled_sites$data$AllSites$verbatimSRS) %in% c("EPSG:4269", NA_character_))
+  expect_all_true(unique(mojn_wrangled_sites$data$AllSites$geodeticDatum) %in% c("EPSG:4326", NA_character_))
 
   rows_w_coord <- mojn_wrangled_sites$data$AllSites %>% dplyr::filter(!is.na(verbatimCoordinates))
-  expect_true(all(stringr::str_detect(rows_w_coord$verbatimCoordinates[rows_w_coord$Park == "GRBA"], "^11N ")))
-  expect_true(all(stringr::str_detect(rows_w_coord$verbatimCoordinates[rows_w_coord$Park == "PARA"], "^12N ")))
-  expect_true(all(mojn_wrangled_sites$data$AllSites$verbatimCoordinateSystem == "UTM"))
-  expect_true(all(mojn_wrangled_sites$data$AllSites$verbatimSRS == "EPSG:4269"))
-  expect_true(all(mojn_wrangled_sites$data$AllSites$geodeticDatum == "EPSG:4326"))
+  expect_all_true(stringr::str_detect(rows_w_coord$verbatimCoordinates[rows_w_coord$Park == "GRBA"], "^11N "))
+  expect_all_true(stringr::str_detect(rows_w_coord$verbatimCoordinates[rows_w_coord$Park == "PARA"], "^12N "))
+  expect_all_true(mojn_wrangled_sites$data$AllSites$verbatimCoordinateSystem == "UTM")
+  expect_all_true(mojn_wrangled_sites$data$AllSites$verbatimSRS == "EPSG:4269")
+  expect_all_true(mojn_wrangled_sites$data$AllSites$geodeticDatum == "EPSG:4326")
 
   # Test column additions/renames are present
-  expect_true(all(c("shift", "verbatimCoordinates", "verbatimCoordinateSystem", "verbatimSRS", "geodeticDatum", "standHeightInMeters",
-                    "decimalLatitude", "decimalLongitude", "elevationInMeters", "slopeInPercent", "aspectInDegrees") %in% names(mojn_wrangled_sites$data$AllSites)))
+  expect_contains(names(mojn_wrangled_sites$data$AllSites), c("shift", "verbatimCoordinates", "verbatimCoordinateSystem", "verbatimSRS", "geodeticDatum", "standHeightInMeters", "decimalLatitude", "decimalLongitude", "elevationInMeters", "slopeInPercent", "aspectInDegrees"))
 })
 
 # wrangleSiteVisit
@@ -41,12 +40,12 @@ test_that("wrangleSiteVisit works", {
   mojn_wrangled_sitevisit <- mojn_wrangled %>% wrangleSites() %>% wrangleSiteVisit()
 
   # Test that park codes and protocol version values are expanded
-  expect_true(all(unique(mojn_wrangled_sitevisit$data$SiteVisit$protocolVersion) %in% c("Aspen PIP 1.0", NA_character_))) # only 1 value in lookup tbl
-  expect_true(all(unique(mojn_wrangled_sitevisit$data$SiteVisit$unitName) %in% c("Great Basin National Park", "Grand Canyon-Parashant National Monument", NA_character_)))
+  expect_all_true(unique(mojn_wrangled_sitevisit$data$SiteVisit$protocolVersion) %in% c("Aspen PIP 1.0", NA_character_)) # only 1 value in lookup tbl
+  expect_all_true(unique(mojn_wrangled_sitevisit$data$SiteVisit$unitName) %in% c("Great Basin National Park", "Grand Canyon-Parashant National Monument", NA_character_))
 
   # Test column additions/renames are present
   joined_names <- lubridate::setdiff(names(mojn_wrangled_sitevisit$data$AllSites), c("Park", "Site", "ParkName"))
-  expect_true(all(c(joined_names, "eventDate", "protocolVersion", "unitCode", "unitName", "siteID", "recordedBy") %in% names(mojn_wrangled_sitevisit$data$SiteVisit)))
+  expect_contains(names(mojn_wrangled_sitevisit$data$SiteVisit), c(joined_names, "eventDate", "protocolVersion", "unitCode", "unitName", "siteID", "recordedBy"))
   })
 
 # wrangleDisturbance
@@ -55,26 +54,26 @@ test_that("wrangleDisturbances works", {
   mojn_wrangled_disturbance <- mojn_wrangled %>% wrangleSites() %>% wrangleSiteVisit() %>% wrangleDisturbances()
 
   # Test that disturbance codes are expanded
-  expect_true(all(unique(mojn_wrangled_disturbance$data$Disturbance$disturbance) %in% c("Antler rubbing", "Fire", "Livestock grazing", "Wildlife grazing", "Arborglyph", NA_character_)))
+  expect_all_true(unique(mojn_wrangled_disturbance$data$Disturbance$disturbance) %in% c("Antler rubbing", "Fire", "Livestock grazing", "Wildlife grazing", "Arborglyph", NA_character_))
 
   # Test column additions/renames are present
-  expect_true(all(c("unitCode", "unitName", "siteID", "eventDate", "VisitType", "Community", "disturbance") %in% names(mojn_wrangled_disturbance$data$Disturbance)))
+  expect_contains(names(mojn_wrangled_disturbance$data$Disturbance), c("unitCode", "unitName", "siteID", "eventDate", "VisitType", "Community", "disturbance"))
   })
+
 
 # wrangleObservations
 test_that("wrangleObservations works", {
   skip_if_not(file.exists(mojn_wrangle_test_data), "Skipping tests for wrangleObservations, MOJN data for testing wrangle functions not available.")
   mojn_wrangled_observations <- mojn_wrangled %>% wrangleSites() %>% wrangleSiteVisit() %>% wrangleDisturbances() %>% wrangleObservations()
 
-  # Test column additions/renames are present
-  expect_true(all(c("unitCode", "unitName", "siteID", "eventDate", "VisitType", "Community", "verbatimIdentification",
-                    "sizeClass", "individualCount", "sizeClassDescription") %in% names(mojn_wrangled_observations$data$Observations)))
-
   # Test expanding size classes and sci names
-  expect_true(all(c("Class I", "Class II", "Class III", "Class IV", "Class V", "Class VI") %in% unique(mojn_wrangled_observations$data$Observations$sizeClass)))
+  expect_all_true(c("Class I", "Class II", "Class III", "Class IV", "Class V", "Class VI") %in% unique(mojn_wrangled_observations$data$Observations$sizeClass))
   expect_false(any(is.na(mojn_wrangled_observations$data$Observations$sizeClass)))
   expect_false(any(is.na(mojn_wrangled_observations$data$Observations$sizeClassDescription)))
   expect_false(any(stringr::str_detect(unique(mojn_wrangled_observations$data$Observations$verbatimIdentification), "\\(|\\)")))
+
+  # Test column additions/renames are present
+  expect_contains(names(mojn_wrangled_observations$data$Observations), c("unitCode", "unitName", "siteID", "eventDate", "VisitType", "Community", "verbatimIdentification", "sizeClass", "individualCount", "sizeClassDescription"))
 
   # Test that col globalid is removed from SiteVisits tbl
   expect_false("globalid" %in% names(mojn_wrangled_observations$data$SiteVisit))
@@ -86,7 +85,7 @@ test_that("wranglePests works", {
   mojn_wrangled_pests <- mojn_wrangled %>% wrangleSites() %>% wrangleSiteVisit() %>% wrangleDisturbances() %>% wrangleObservations() %>% wranglePests()
 
   # Test column additions/renames are present
-  expect_true(all(c("unitCode", "unitName", "siteID", "eventDate", "VisitType", "Community", "SpeciesCode", "verbatimIdentification", "pest") %in% names(mojn_wrangled_pests$data$Pests)))
+  expect_contains(names(mojn_wrangled_pests$data$Pests), c("unitCode", "unitName", "siteID", "eventDate", "VisitType", "Community", "SpeciesCode", "verbatimIdentification", "pest"))
 
   # Test that col globalid is removed from observations tbl
   expect_false("globalid" %in% names(mojn_wrangled_pests$data$Observations))
